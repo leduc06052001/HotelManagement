af3bc7dc6b99861c9f020f7184e3b625e609b3f7
leduc <leduc@DESKTOP-1T8AV99>
2023-09-12 14:13:43 +0700

    Add project files.

---
+++ HM/Controllers/RoomsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QL_khachSan.Controllers
+{
+    public class RoomsController : Controller
+    {
+        // GET: Room
+        public ActionResult Rooms()
+        {
+            return View();
+        }
+
+        public ActionResult Booking()
+        {
+            return View();
+        }
+
+        public ActionResult RoomList()
+        {
+            return View();
+        }
+
+        public ActionResult RoomDetail() 
+        {
+            return View();
+        }
+    }
+}
